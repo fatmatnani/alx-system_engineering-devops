526ef37628637086daceaf2840e84e95d7035f19
fatma tnani <fatmatnani08@gmail.com>
2023-06-26 14:13:21 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,16 @@
-# puppet script - configure ssh authentication:file_line { 'Turn off passwd auth':
+# puppet script - configure ssh authentication:i
+file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
-}file_line { 'Declare identity file':
+}
+
+file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',
-}service { 'ssh':
+}
+
+service { 'ssh':
   ensure => running,
   enable => true,
 }
+
