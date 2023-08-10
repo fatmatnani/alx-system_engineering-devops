254927d54d81ba5d740b431b83e41c21556d72d8
fatma tnani <fatmatnani08@gmail.com>
2023-08-10 07:35:00 -0700

    advanced api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import requests
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'custom-agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    return response.json().get("data", {}).get("subscribers", 0)
